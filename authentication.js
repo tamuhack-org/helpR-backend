722d6ca8fba397693027f2d53635cd16205af9f2
NitroGuy10 <naviyer10@gmail.com>
2022-09-13 15:45:21 -0500

    Axios request fails some times and idk why

--- authentication.js
+++ authentication.js
@@ -10,7 +10,34 @@ export async function getOrMakeUser (request)
         const token = request.headers.authorization.split("Basic ")[1];
         const tokenCheckUrl = "https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=" + token;
         
-        const response = await axios.get(tokenCheckUrl);
+        let response
+        try
+        {
+            response = await axios.get(tokenCheckUrl);
+        }
+        catch (error)
+        {
+            console.log("Axios request failed to GET " + tokenCheckUrl);
+            // TODO this *should* be temporary just to figure out what the error is
+            if (error.response)
+            {
+                console.log("Looks like an HTTP error code was sent");
+                console.log("Data:");
+                console.log(error.response.data);
+                console.log("Status:");
+                console.log(error.response.status);
+                console.log("Headers:");
+                console.log(error.response.headers);
+            }
+            else if (error.request)
+            {
+                console.log("Looks like no response was received");
+                console.log("Request:");
+                console.log(error.request);
+            }
+            return null;
+        }
+
         if (response.data && response.data.email)
         {
             let user = await db.getUserByEmail(response.data.email);
